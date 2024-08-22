44a0c7bc8c71373ecf0bcc2aacedf41f467beda0
Rediet_W <vivezlaviequetuaimes@gmail.com>
2024-08-22 18:00:01 +0300

    edited the navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import Image from "next/image";
+import JobList from "./jobs/page";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1> on home page</h1>
-      </div>
-    </main>
-  );
+  return <JobList />;
 }
